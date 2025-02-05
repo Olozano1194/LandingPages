bfa78d81424544304ebf2349a69924a278851ab7
Oscar Lozano <olozano99@itfip.edu.co>
2025-02-05 01:37:10 -0500

    se le modificarón algunos estilos a los menus en el header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import NavBar from './navBar/NavBar';
 const Header = () => {
     return (
         <header className="w-full flex justify-between items-center py-4 px-4">
-            <h1>Header</h1>
+            <img src="" alt="img-logo" />
             <NavBar />
         </header>
     );
--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -29,38 +29,38 @@ const NavBar = () => {
         <nav className="flex justify-between items-center">
             {/* Inicio */}
             <ul>
-                <li><Link to="#" className="flex items-center gap-2"><FaHome />Inicio</Link></li>
+                <li><Link to="#" className="flex items-center gap-2 rounded-lg py-2 px-4 hover:bg-slate-200"><FaHome />Inicio</Link></li>
             </ul>
             {/* Joyas */}
-            <ul>
+            <ul className="relative">
                 <button onClick={() => handleToggleSubMenu('menu1')} className="w-full flex items-center justify-between py-2 px-4 rounded-lg hover:bg-slate-200 text-dark font-semibold transition-colors"><span className="flex items-center gap-2"><FaGem />Joyas</span><RiArrowRightSLine className={`mt-1 ${showSubMenu.menu1 ? 'rotate-90': ''} transition-all`} />
                 </button>
-                <ul className={`mt-2 ${!showSubMenu.menu1 ? 'hidden' : ''}`}>
-                    <li><Link className="" to="#">Relojes</Link></li>
-                    <li><Link className="" to="#">Manillas</Link></li>
-                    <li><Link className="" to="#">Aretes</Link></li>
-                    <li><Link className="" to="#">Anillos</Link></li>
-                    <li><Link className="" to="#">Collares</Link></li>
-                    <li><Link className="" to="#">Joyas</Link></li>
+                <ul className={`absolute top-full left-0 mt-2 ${!showSubMenu.menu1 ? 'hidden' : ''}`}>
+                    <li><Link className="block px-4 py-2 hover:bg-slate-100" to="#">Relojes</Link></li>
+                    <li><Link className="block px-4 py-2 hover:bg-slate-100" to="#">Manillas</Link></li>
+                    <li><Link className="block px-4 py-2 hover:bg-slate-100" to="#">Aretes</Link></li>
+                    <li><Link className="block px-4 py-2 hover:bg-slate-100" to="#">Anillos</Link></li>
+                    <li><Link className="block px-4 py-2 hover:bg-slate-100" to="#">Collares</Link></li>
+                    <li><Link className="block px-4 py-2 hover:bg-slate-100" to="#">Joyas</Link></li>
                 </ul>
                 
             </ul>
             {/* Servicios */}
-            <ul>
-                <button onClick={() => handleToggleSubMenu('menu2')} className="w-full flex items-center justify-between py-2 px-4 rounded-lg hover:bg-slate-200 text-dark font-semibold transition-colors"><span className="flex items-center gap-2"><FaTools />Servicios</span><RiArrowRightSLine className={`mt-1 ${showSubMenu.menu1 ? 'rotate-90': ''} transition-all`} />
+            <ul className="relative">
+                <button onClick={() => handleToggleSubMenu('menu2')} className="w-full flex items-center justify-between py-2 px-4 rounded-lg hover:bg-slate-200 text-dark font-semibold transition-colors"><span className="flex items-center gap-2"><FaTools />Servicios</span><RiArrowRightSLine className={`mt-1 ${showSubMenu.menu2 ? 'rotate-90': ''} transition-all`} />
                 </button>
-                <ul className={`mt-2 ${!showSubMenu.menu2 ? 'hidden' : ''}`}>
-                    <li><Link className="" to="#">Relojes</Link></li>
-                    <li><Link className="" to="#">Manillas</Link></li>
-                    <li><Link className="" to="#">Aretes</Link></li>
-                    <li><Link className="" to="#">Anillos</Link></li>
-                    <li><Link className="" to="#">Collares</Link></li>
-                    <li><Link className="" to="#">Joyas</Link></li>
+                <ul className={`absolute top-full left-0 mt-2 ${!showSubMenu.menu2 ? 'hidden' : ''}`}>
+                    <li><Link className="block px-4 py-2 hover:bg-slate-100" to="#">Relojes</Link></li>
+                    <li><Link className="block px-4 py-2 hover:bg-slate-100" to="#">Manillas</Link></li>
+                    <li><Link className="block px-4 py-2 hover:bg-slate-100" to="#">Aretes</Link></li>
+                    <li><Link className="block px-4 py-2 hover:bg-slate-100" to="#">Anillos</Link></li>
+                    <li><Link className="block px-4 py-2 hover:bg-slate-100" to="#">Collares</Link></li>
+                    <li><Link className="block px-4 py-2 hover:bg-slate-100" to="#">Joyas</Link></li>
                 </ul>                
             </ul>
             {/* Contactos */}
             <ul>
-                <li><Link to="#"><FaPhone />Contáctanos</Link></li>
+                <li><Link to="#" className="flex items-center gap-2 rounded-lg py-2 px-4 hover:bg-slate-200"><FaPhone />Contáctanos</Link></li>
             </ul>
         </nav>
     );
